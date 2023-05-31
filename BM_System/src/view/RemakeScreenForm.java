6a9223f972d38e787b6fd6d2734e0f9657a86f5c
Admin <Admin@example>
2023-05-31 12:28:04 +0700

    set frame cho chuyển

---
+++ BM_System/src/view/RemakeScreenForm.java
@@ -0,0 +1,21 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package view;
+
+import java.awt.LayoutManager;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class RemakeScreenForm {
+    public RemakeScreenForm() {
+    }
+
+    public void remakeScreen(JFrame frame, JPanel f) {
+        f.setLayout((LayoutManager)null);
+        frame.setContentPane(f);
+        frame.setVisible(true);
+    }
+}
